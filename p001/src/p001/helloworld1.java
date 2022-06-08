5bc07600128133992920283f4778ed01df74e14a
21008334 <21008334@LAPTOP-IJV9B935>
2022-06-08 13:59:29 +0800

    commit by jun hong

---
+++ p001/src/p001/helloworld1.java
@@ -0,0 +1,10 @@
+package p001;
+
+public class helloworld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("my first java program");
+	}
+
+}
